05b086792e09d7646accd7b260209e998cb312b5
Ilya Konstantinov <ilya.konstantinov@gmail.com>
2015-04-19 00:30:48 +0300

    applemedia: implement copying of meta
    Before this, buffers would lose their Core Video / Core Media meta
    over intervideo* boundary.

--- sys/applemedia/coremediabuffer.c
+++ sys/applemedia/coremediabuffer.c
@@ -23,17 +23,69 @@
 #include "coremediabuffer.h"
 #include "corevideomemory.h"
 
+static const GstMetaInfo *gst_core_media_meta_get_info (void);
+
 static void
-gst_core_media_meta_free (GstCoreMediaMeta * meta, GstBuffer * buf)
+gst_core_media_meta_add (GstBuffer * buffer, CMSampleBufferRef sample_buf,
+    CVImageBufferRef image_buf, CMBlockBufferRef block_buf)
 {
+  GstCoreMediaMeta *meta;
+
+  meta =
+      (GstCoreMediaMeta *) gst_buffer_add_meta (buffer,
+      gst_core_media_meta_get_info (), NULL);
+  CFRetain (sample_buf);
+  if (image_buf)
+    CVBufferRetain (image_buf);
+  if (block_buf)
+    CFRetain (block_buf);
+  meta->sample_buf = sample_buf;
+  meta->image_buf = image_buf;
+  meta->block_buf = block_buf;
+  if (image_buf != NULL && CFGetTypeID (image_buf) == CVPixelBufferGetTypeID ())
+    meta->pixel_buf = (CVPixelBufferRef) image_buf;
+  else
+    meta->pixel_buf = NULL;
+}
+
+static void
+gst_core_media_meta_free (GstMeta * gmeta, GstBuffer * buf)
+{
+  GstCoreMediaMeta *meta;
+
+  meta = (GstCoreMediaMeta *) gmeta;
+
   if (meta->image_buf != NULL) {
     CVBufferRelease (meta->image_buf);
   }
+
   if (meta->block_buf != NULL) {
     CFRelease (meta->block_buf);
   }
 
   CFRelease (meta->sample_buf);
+}
+
+static gboolean
+gst_core_media_meta_transform (GstBuffer * transbuf, GstMeta * gmeta,
+    GstBuffer * buffer, GQuark type, gpointer data)
+{
+  GstMetaTransformCopy *copy_data;
+  GstCoreMediaMeta *meta;
+
+  meta = (GstCoreMediaMeta *) gmeta;
+  copy_data = (GstMetaTransformCopy *) data;
+
+  if (!copy_data->region) {
+    /* only copy if the complete data is copied as well */
+    gst_core_media_meta_add (transbuf, meta->sample_buf, meta->image_buf,
+        meta->block_buf);
+  } else {
+    GST_WARNING_OBJECT (transbuf,
+        "dropping Core Media metadata due to partial buffer");
+  }
+
+  return TRUE;                  /* retval unused */
 }
 
 GType
@@ -57,9 +109,9 @@ gst_core_media_meta_get_info (void)
   if (g_once_init_enter (&core_media_meta_info)) {
     const GstMetaInfo *meta = gst_meta_register (GST_CORE_MEDIA_META_API_TYPE,
         "GstCoreMediaMeta", sizeof (GstCoreMediaMeta),
-        (GstMetaInitFunction) NULL,
-        (GstMetaFreeFunction) gst_core_media_meta_free,
-        (GstMetaTransformFunction) NULL);
+        /* (GstMetaInitFunction) */ NULL,
+        /* (GstMetaFreeFunction) */ gst_core_media_meta_free,
+        /* (GstMetaTransformFunction) */ gst_core_media_meta_transform);
     g_once_init_leave (&core_media_meta_info, meta);
   }
   return core_media_meta_info;
@@ -184,7 +236,6 @@ gst_core_media_buffer_new (CMSampleBufferRef sample_buf,
 {
   CVImageBufferRef image_buf;
   CMBlockBufferRef block_buf;
-  GstCoreMediaMeta *meta;
   GstBuffer *buf;
 
   gst_core_video_memory_init ();
@@ -194,29 +245,18 @@ gst_core_media_buffer_new (CMSampleBufferRef sample_buf,
 
   buf = gst_buffer_new ();
 
-  meta = (GstCoreMediaMeta *) gst_buffer_add_meta (buf,
-      gst_core_media_meta_get_info (), NULL);
-  CFRetain (sample_buf);
-  if (image_buf)
-    CVBufferRetain (image_buf);
-  if (block_buf)
-    CFRetain (block_buf);
-  meta->sample_buf = sample_buf;
-  meta->image_buf = image_buf;
-  meta->pixel_buf = NULL;
-  meta->block_buf = block_buf;
+  gst_core_media_meta_add (buf, sample_buf, image_buf, block_buf);
 
   if (image_buf != NULL && CFGetTypeID (image_buf) == CVPixelBufferGetTypeID ()) {
     GstVideoInfo info;
     gboolean has_padding = FALSE;
+    CVPixelBufferRef pixel_buf = (CVPixelBufferRef) image_buf;
 
-    meta->pixel_buf = (CVPixelBufferRef) image_buf;
-    if (!gst_video_info_init_from_pixel_buffer (&info, meta->pixel_buf)) {
+    if (!gst_video_info_init_from_pixel_buffer (&info, pixel_buf)) {
       goto error;
     }
 
-    gst_core_video_wrap_pixel_buffer (buf, &info, meta->pixel_buf, &has_padding,
-        map);
+    gst_core_video_wrap_pixel_buffer (buf, &info, pixel_buf, &has_padding, map);
 
     /* If the video meta API is not supported, remove padding by
      * copying the core media buffer to a system memory buffer */
